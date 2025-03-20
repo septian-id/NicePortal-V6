f2d9eea2972dac4c1abcfad47535502b24e7c437
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2025-03-20 13:08:44 +0700

    Add files via upload

---
+++ assets/js/system.js
@@ -0,0 +1,101 @@
+class System {
+    constructor() {
+        this.config = new Config();
+        this.server = 'https://accounts.niceportal.net';
+        this.params = {
+            "user_id": this.config.user_id,
+            "router_id": this.config.router_id
+        }
+        this.config.session = 'off';
+    }
+    
+    async get_info(){
+        return new Promise(resolve => {
+            $.ajax({
+                url: `${this.server}/api/info`,
+                type: 'POST',
+                data: JSON.stringify(this.params),
+                success: (result) => {
+                    resolve(result);
+                },
+                error: (err) => {
+                    resolve(err);
+                }
+            });
+        });
+    }
+
+    async get_product(id = null) {
+        return new Promise(resolve => {
+            if (id !== null) {
+                this.params.product_id = id;
+            }
+            $.ajax({
+                url: `${this.server}/api/product`,
+                type: 'POST',
+                data: JSON.stringify(this.params),
+                success: (result) => {
+                    resolve(result);
+                },
+                error: (err) => {
+                    resolve(err);
+                }
+            });
+        });
+    }
+
+    async checkout(data) {
+        return new Promise(resolve => {
+            this.params.product_id = data.product_id;
+            this.params.first_name = data.first_name;
+            this.params.last_name = data.last_name;
+            this.params.phone = data.phone;
+            this.params.email = data.email;
+            $.ajax({
+                url: `${this.server}/api/checkout`,
+                type: 'POST',
+                data: JSON.stringify(this.params),
+                success: (result) => {
+                    resolve(result);
+                },
+                error: (err) => {
+                    resolve(err);
+                }
+            });
+        });
+    }
+    
+    async get_status(order_id){
+        return new Promise(resolve => {
+            this.params.order_id = order_id;
+            $.ajax({
+                url: `${this.server}/api/status`,
+                type: 'POST',
+                data: JSON.stringify(this.params),
+                success: (result) => {
+                    resolve(result);
+                },
+                error: (err) => {
+                    resolve(err);
+                }
+            });
+        });
+    }
+
+    async clear_session(username = null){
+        return new Promise(resolve => {
+            this.params.username = username;
+            $.ajax({
+                url: `${this.server}/api/session`,
+                type: 'POST',
+                data: JSON.stringify(this.params),
+                success: (result) => {
+                    resolve(result);
+                },
+                error: (err) => {
+                    resolve(err);
+                }
+            });
+        });
+    }
+}
---
+++ config.js
@@ -0,0 +1,8 @@
+class Config{
+    constructor(){
+        this.user_id = '72SGPJZV22H07UJQ';
+        this.router_id = 'Q2J24W13IY8JKOPR';
+        
+        this.color = 'theme-green.css';
+    }
+}
