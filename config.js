e059e42858aa2f59f6d51a323f0b2ef9ff9adc1e
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2025-04-15 23:01:35 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
-class Config{
-    constructor(){
-        this.user_id = '72SGPJZV22H07UJQ';
-        this.router_id = 'Q2J24W13IY8JKOPR';
-        
-        this.color = 'theme-green.css';
-    }
+var config = {
+	user_id: '72SGPJZV22H07UJQ',
+	router_id: 'Q2J24W13IY8JKOPR',
+	server: 'https://niceportal.net'
 }
